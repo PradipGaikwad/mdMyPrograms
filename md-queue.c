3c310af5bf22b19e20d35e2826c95d42db7694a9
pradip <pradgaik@cisco.com>
2021-08-06 22:33:00 +0530

    fixed queue size issue

--- md-queue.c
+++ md-queue.c
@@ -14,7 +14,7 @@ struct queue {
 
 void dump(struct queue *q) {
     if(!q) return ;
-    printf("head:tail - %d:%d \n", q->head, q->tail);
+    printf("head:tail - %d:%d->%d\n", q->head, q->tail, q->sz);
     for(int i=0;i<max_sz; i++) {
         printf("%c ->", q->data[i]);
     }
@@ -29,11 +29,15 @@ void push(struct queue *q, const char *udata) {
         return;
     }
 
-    q->sz += 1;
     
     for(int i=0;i<strlen(udata); i++) {
         q->tail = (q->tail + 1) % max_sz;
-        if(q->tail == q->head) q->head++;
+
+        if (q->tail == q->head) 
+            q->head++;
+        else 
+            q->sz = q->sz+1;
+        
         q->data[q->tail] = udata[i];
         if(q->head==-1) q->head=0;
     }
@@ -50,6 +54,23 @@ struct queue* initq() {
     return q;
 }
 
+void pop(struct queue *q) 
+{
+    if(q->sz==0) {
+        printf("queue is empty \n");
+        return;
+    }
+
+    printf("popping - %c\n", q->data[q->head]);
+    
+    if(q->head == q->tail) 
+        q->head=q->tail=-1;
+    else
+        q->head = (q->head+1) % max_sz;
+    
+    q->sz -= 1;
+    dump(q);
+}
 int main()
 {
     struct queue *q; 
@@ -59,4 +80,6 @@ int main()
     push(q,"EFGH");
     push(q,"IJKLM");
     push(q,"abcd");
+    
+    for(int i=0;i<11; i++) pop(q);
 }
